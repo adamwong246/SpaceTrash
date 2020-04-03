ffd4f1cdefbfcbc10f0d110a48db28aafd4115cc
adam wong <adamwong246@gmail.com>
2020-04-03 12:59:14 -0700

    cleanup

--- src/renderer/redux/initialState.ts
+++ src/renderer/redux/initialState.ts
@@ -119,26 +119,27 @@ module.exports =  {
     y: 0,
     mapCell: {}
   },
-  ships: [{
-    id: 0,
-    name: "USCSS Nostromo",
-    map: new RayCastMap(16, 16),
-    class: "Starfreighter",
-    registration: "1809246(09)",
-    model: "Lockmart CM-88B Bison",
-    make: "Weyland-Yutani",
-    year: "2064",
-    files: {
-      'makeNostromo.ts': "hello world",
-      'makeDrone.ts': "hello makeDrone.ts",
-      'utils': {
-        'foo.js': "hello foo.ts",
-        'bar.js': "hello bar.ts",
-        'dig.js': "hello dig.ts",
+  ships: [
+    {
+      id: 0,
+      name: "USCSS Nostromo",
+      map: new RayCastMap(16, 16),
+      class: "Starfreighter",
+      registration: "1809246(09)",
+      model: "Lockmart CM-88B Bison",
+      make: "Weyland-Yutani",
+      year: "2064",
+      files: {
+        'makeNostromo.ts': "hello world",
+        'makeDrone.ts': "hello makeDrone.ts",
+        'utils': {
+          'foo.js': "hello foo.ts",
+          'bar.js': "hello bar.ts",
+          'dig.js': "hello dig.ts",
+        }
       }
-    }
-  },
-  {
+    },
+    {
     id: 1,
     name: "USCSS Sulaco",
     map: new RayCastMap(16, 16),
@@ -151,7 +152,7 @@ module.exports =  {
       'makeSulaco.ts': "// implement a class that inheirits from ship.ts\nmodule.exports = {}"
     }
   },
-  {
+    {
     id: 2,
     name: "IPU Merkava",
     map: new RayCastMap(16, 16),
@@ -175,36 +176,36 @@ module.exports =  {
   boardedShip: 1,
   editingShip: 0,
   editingFile: [],
-  drones: [{
-    id: 0,
-    name: "Larry",
-    upgrades: [0, 1],
-    x: 7,
-    y: 12,
-    direction: 0,
-    commandQueue: []
-  },
-  {
-    id: 1,
-    name: "Curly",
-    upgrades: [2, 3, 4],
-    x: 6,
-    y: 11,
-    direction: 6,
-    commandQueue: []
-  },
-  {
-    id: 2,
-    name: "Moe",
-    upgrades: [5],
-    x: 5,
-    y: 11,
-    direction: 9,
-    commandQueue: []
-  }
+  drones: [
+    {
+      id: 0,
+      name: "Larry",
+      upgrades: [0, 1],
+      x: 7,
+      y: 12,
+      direction: 0,
+      commandQueue: []
+    },
+    {
+      id: 1,
+      name: "Curly",
+      upgrades: [2, 3, 4],
+      x: 6,
+      y: 11,
+      direction: 6,
+      commandQueue: []
+    },
+    {
+      id: 2,
+      name: "Moe",
+      upgrades: [5],
+      x: 5,
+      y: 11,
+      direction: 9,
+      commandQueue: []
+    }
   ],
   droneWithActiveVideo: 0,
-
   camera: {
     x: 0, //left
     y: 1000, // up
