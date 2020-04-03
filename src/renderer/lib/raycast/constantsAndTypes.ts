723dc8012210e03e7cafee28bd9135f6f77e9b2d
adam wong <adamwong246@gmail.com>
2020-04-03 09:02:50 -0700

    update

--- src/renderer/lib/raycast/constantsAndTypes.ts
+++ src/renderer/lib/raycast/constantsAndTypes.ts
@@ -32,14 +32,10 @@ export interface IStrip {
 
 export const emptyStrip: IStrip = {
 	style: emptyStyle,
-
 }
 
 var spriteMap: any[][] = [[]];
 var visibleSprites: ISprite[] = [];
-
-
-
 var oldVisibleSprites = [];
 
 export const screenWidth = 320;
@@ -48,8 +44,7 @@ export const screenHeight = 200;
 var useSingleTexture = false;
 var fov = 60 * Math.PI / 180;
 
-
-export const stripWidth = 10;
+export const stripWidth = 3;
 
 export const numRays = Math.ceil(screenWidth / stripWidth);
 var numTextures = 4;
@@ -59,7 +54,6 @@ var wallTextures = [
 	"walls_3.png",
 	"walls_4.png"
 ];
-
 
 export const viewDist = (screenWidth/2) / Math.tan((fov / 2));
 export const twoPI = Math.PI * 2;
