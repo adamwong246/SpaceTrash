ae5acd1524c86fb12814f4e3452a128f59c125b7
adam wong <adamwong246@gmail.com>
2020-04-04 20:42:19 -0700

    auto-scroll

--- src/renderer/components/Terminal.tsx
+++ src/renderer/components/Terminal.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {createRef}  from 'react';
 import { connect } from "react-redux";
 
 import {getTerminalProps} from "../redux/selectors";
@@ -8,6 +9,9 @@ class Terminal extends React.Component<{
 }, {
   value: string;
 }> {
+
+  private messagesEndRef = createRef<HTMLDivElement>()
+
   constructor(a) {
     super(a);
 
@@ -18,6 +22,21 @@ class Terminal extends React.Component<{
     this.handleChange = this.handleChange.bind(this);
   }
 
+
+
+
+  componentDidMount () {
+    this.scrollToBottom()
+  }
+  componentDidUpdate () {
+    this.scrollToBottom()
+  }
+  scrollToBottom = () => {
+    if(this.messagesEndRef.current){
+      this.messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+
+  }
 
   handleChange(event) {
     const {value} = event.target;
@@ -31,7 +50,7 @@ class Terminal extends React.Component<{
       <div id="terminal">
 
       {this.props.logs.map((c, ndx) => <p key={`terminal-line-${ndx}`}>{c}</p>)}
-
+      <div ref={this.messagesEndRef} />
     </div>);
   }
 }
