491a58b23ddc02f2cf7b3f05f1dea35ae95a851a
adam wong <adamwong246@gmail.com>
2020-08-23 14:06:58 -0700

    redirect on login

--- src/node-express-passport-mongoose-auth/controllers/AuthController.js
+++ src/node-express-passport-mongoose-auth/controllers/AuthController.js
@@ -35,7 +35,8 @@ userController.login = function(req, res) {
 // Post login
 userController.doLogin = function(req, res) {
   passport.authenticate('local')(req, res, function () {
-    res.redirect('/');
+    // res.redirect('/');
+    res.redirect(req.header('Referer') || '/');
   });
 };
 
